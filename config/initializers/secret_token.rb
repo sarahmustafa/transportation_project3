a940f00d9b6c129ea6b94f551e905b7112b4b3a0
Sarah Mustafa <sarahmustafa@Sarahs-MacBook-Pro.local>
2012-11-23 11:41:25 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TransportationProject3::Application.config.secret_token = 'efa6af0628761fea1aea047988372a9cc1baa74693d486cfed981cbff2e8ef73fd34a0594b2fc9eda7681aff67e49e78e74136f249e13d2aa9691b221a00f82b'
